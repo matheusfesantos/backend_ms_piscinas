42567375eedfcf440eceb1a3331e273f9560dcc6
matheusfesantos <matheusfgsantos31@gmail.com>
2025-03-20 16:35:40 -0300

    melhorando a estrutura da entidade Cleintes para corpos em JSON

--- src/main/java/com/example/ms_pscinas/entitys/Clientes.java
+++ src/main/java/com/example/ms_pscinas/entitys/Clientes.java
@@ -1,15 +1,13 @@
 package com.example.ms_pscinas.entitys;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "clientes")
 public class Clientes {
 
     @Id
